a017495f0ee4203f494313e303dba8b84dc18320
danyal <danyallsty04@gmail.com>
2023-11-25 04:46:38 -0800

    .

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,38 +1,37 @@
-'use client';
+"use client";
 
-import {motion} from 'framer-motion';
-import styles from '../styles'
-import {navVariants} from '../utils/motion'
-import Image from 'next/image'
-import search from "../public/search.svg"
-import menu from "../public/menu.svg"
-
+import { motion } from "framer-motion";
+import styles from "../styles";
+import { navVariants } from "../utils/motion";
+import Image from "next/image";
+import search from "../public/search.svg";
+import menu from "../public/menu.svg";
 
 const Navbar = () => (
   <motion.nav
-  variants={navVariants}
-  initial='hidden'
-  whileInView='show'
-  className={`${styles.xPaddings} py-8 relative overflow-hidden`}
-
-  >
-
-    <div className='absolute w-[50%] inset-0 gradient-01  ' />
-    <div className={`${styles.innerwidth} mx-auto flex justify-between gap-8
+    variants={navVariants}
+    initial="hidden"
+    whileInView="show"
+    className={`${styles.xPaddings} py-8 relative overflow-hidden`}>
+    <div className="absolute w-[50%] inset-0 gradient-01  " />
+    <div
+      className={`${styles.innerwidth} mx-auto flex justify-between gap-8
     `}>
-      <Image src={search} alt="search"
-      className='w-[24px] h-[24px] object-contain'
+      <Image
+        src={search}
+        alt="search"
+        className="w-[24px] h-[24px] object-contain"
       />
-      
-      <h2 className='font-extrabold text-[24px] leading-[30px] text-white '>METAVERSUS</h2>
-      <Image 
-      src={menu}
-      alt='menu'
-      className='w-[24px] h-[24px] object-contain '
+
+      <h2 className="font-extrabold text-[24px] leading-[30px] text-white ">
+        METAVERSUS
+      </h2>
+      <Image
+        src={menu}
+        alt="menu"
+        className="w-[24px] h-[24px] object-contain "
       />
-      
     </div>
-      
   </motion.nav>
 );
 
