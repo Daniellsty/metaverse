c00ee8f66e56760ba16475f64a9319e2776ed7ad
danyal <danyallsty04@gmail.com>
2023-11-25 01:22:31 -0800

    navbar complted

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,22 +13,26 @@ const Navbar = () => (
   variants={navVariants}
   initial='hidden'
   whileInView='show'
-  className={`${styles.xPaddings} py-8 relative`}
+  className={`${styles.xPaddings} py-8 relative overflow-hidden`}
 
   >
+
     <div className='absolute w-[50%] inset-0 gradient-01  ' />
     <div className={`${styles.innerwidth} mx-auto flex justify-between gap-8
     `}>
       <Image src={search} alt="search"
       className='w-[24px] h-[24px] object-contain'
       />
+      
       <h2 className='font-extrabold text-[24px] leading-[30px] text-white '>METAVERSUS</h2>
       <Image 
       src={menu}
       alt='menu'
       className='w-[24px] h-[24px] object-contain '
       />
+      
     </div>
+      
   </motion.nav>
 );
 
